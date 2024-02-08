40f9c7f69b47ce711eb1043bc21b47bdeb73e0f5
wirabudhi <bpantaiselatan@gmail.com>
2024-02-08 23:57:36 +0800

    Add NasabahSeeder to DatabaseSeeder

---
+++ database/factories/NasabahFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Nasabah>
+ */
+class NasabahFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => $this->faker->randomElement([1, 2, 3]),
+            'alamat' => $this->faker->address,
+            'jenis_kelamin' => $this->faker->title,
+            'status' => $this->faker->randomElement(['Aktif', 'Nonaktif']),
+        ];
+    }
+}
