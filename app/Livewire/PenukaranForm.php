9ef516bf2744bcf01ef91c864d853a467d73b776
wirabudhi <bpantaiselatan@gmail.com>
2024-02-11 15:25:00 +0800

    Refactor PenukaranForm component and penukaran-form.blade.php

--- app/Livewire/PenukaranForm.php
+++ app/Livewire/PenukaranForm.php
@@ -2,12 +2,82 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
+use App\Models\Barang;
+use App\Models\Penukaran;
+use App\Models\Tabungan;
+use Illuminate\Support\Facades\Log;
+use LivewireUI\Modal\ModalComponent;
+use Masmerise\Toaster\Toastable;
 
-class PenukaranForm extends Component
+class PenukaranForm extends ModalComponent
 {
+    use Toastable;
+
+    public Penukaran $penukaran;
+    public $tabungan, $barang, $tabungan_id, $barang_id;
+
+    protected $rules = [
+        'tabungan_id' => 'required|exists:tabungan,id',
+        'barang_id' => 'required|exists:barang,id',
+    ];
+
     public function render()
     {
-        return view('livewire.penukaran');
+        $tabungan = Tabungan::with('nasabah.user')->get();
+        $barang = Barang::all();
+        return view('livewire.penukaran-form', compact('tabungan', 'barang'));
+    }
+
+    public function resetCreateForm()
+    {
+        $this->reset(['tabungan_id', 'barang_id']);
+    }
+
+    public function store()
+    {
+        $validatedData = $this->validate();
+        if (!$this->validateSaldoCukup()) {
+            $this->error('Saldo tidak cukup untuk melakukan penukaran');
+            return;
+        }
+
+        $this->penukaran->fill($validatedData);
+        $this->penukaran->save();
+        $this->success($this->penukaran->wasRecentlyCreated ? 'Penukaran berhasil ditambahkan' : 'Penukaran berhasil diubah');
+
+        $tabungan = Tabungan::find($this->penukaran->tabungan_id);
+
+        $harga = $this->penukaran->barang->harga_barang;
+        $tabungan->updateSaldo($harga, 'kredit', 'Penukaran barang');
+        $this->success('Saldo berhasil dikurangi');
+
+        $this->closeModalWithEvents([
+            PenukaranTable::class => 'penukaranUpdated'
+        ]);
+
+        $this->resetCreateForm();
+    }
+
+    public function mount($rowId = null)
+    {
+        $this->barang = Barang::all();
+        $this->tabungan = Tabungan::with('nasabah.user')->get();
+        $this->penukaran = $rowId ? Penukaran::find($rowId) : new Penukaran;
+        if ($this->penukaran->exists) {
+            $this->tabungan_id = $this->penukaran->tabungan_id;
+            $this->barang_id = $this->penukaran->barang_id;
+        }
+    }
+
+    public function validateSaldoCukup()
+    {
+        $tabungan = Tabungan::find($this->tabungan_id);
+        $barang = Barang::find($this->barang_id);
+
+        if ($tabungan->saldo < $barang->harga_barang) {
+            return false;
+        }
+
+        return true;
     }
 }
--- app/Livewire/PenukaranTable.php
+++ app/Livewire/PenukaranTable.php
@@ -2,9 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Models\HistoryTabungan;
 use App\Models\Penukaran;
+use App\Models\Tabungan;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Builder;
+use Masmerise\Toaster\Toastable;
 use PowerComponents\LivewirePowerGrid\Button;
 use PowerComponents\LivewirePowerGrid\Column;
 use PowerComponents\LivewirePowerGrid\Exportable;
@@ -19,6 +22,7 @@ use PowerComponents\LivewirePowerGrid\Traits\WithExport;
 final class PenukaranTable extends PowerGridComponent
 {
     use WithExport;
+    use Toastable;
 
     public function setUp(): array
     {
@@ -37,7 +41,12 @@ final class PenukaranTable extends PowerGridComponent
 
     public function datasource(): Builder
     {
-        return Penukaran::query();
+        return Penukaran::query()
+            ->join('tabungan', 'penukaran.tabungan_id', '=', 'tabungan.id')
+            ->join('barang', 'penukaran.barang_id', '=', 'barang.id')
+            ->join('nasabah', 'tabungan.nasabah_id', '=', 'nasabah.id')
+            ->join('users', 'nasabah.user_id', '=', 'users.id')
+            ->select('penukaran.*', 'users.name as nama_nasabah', 'barang.nama_barang as nama_barang', 'tabungan.id as tabungan_id', 'barang.id as barang_id');
     }
 
     public function relationSearch(): array
@@ -49,8 +58,8 @@ final class PenukaranTable extends PowerGridComponent
     {
         return PowerGrid::fields()
             ->add('id')
-            ->add('tabungan_id')
-            ->add('barang_id')
+            ->add('nama_nasabah')
+            ->add('nama_barang')
             ->add('created_at');
     }
 
@@ -58,10 +67,8 @@ final class PenukaranTable extends PowerGridComponent
     {
         return [
             Column::make('Id', 'id'),
-            Column::make('Tabungan id', 'tabungan_id'),
-            Column::make('Barang id', 'barang_id'),
-            Column::make('Created at', 'created_at_formatted', 'created_at')
-                ->sortable(),
+            Column::make('Nama Nasabah', 'nama_nasabah'),
+            Column::make('Nama Barang', 'nama_barang'),
 
             Column::make('Created at', 'created_at')
                 ->sortable()
@@ -73,36 +80,137 @@ final class PenukaranTable extends PowerGridComponent
 
     public function filters(): array
     {
-        return [
-        ];
-    }
-
-    #[\Livewire\Attributes\On('edit')]
-    public function edit($rowId): void
-    {
-        $this->js('alert('.$rowId.')');
+        return [];
     }
 
     public function actions(\App\Models\Penukaran $row): array
     {
         return [
             Button::add('edit')
-                ->slot('Edit: '.$row->id)
+                ->slot('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#10b981" class="w-5 h-5">
+                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
+                </svg>
+                ')
                 ->id()
                 ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
-                ->dispatch('edit', ['rowId' => $row->id])
+                ->openModal('penukaran-form', ['rowId' => $row->id]),
+            Button::add('delete')
+                ->slot('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#10b981" class="w-5 h-5">
+                <path d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375Z" />
+                <path fill-rule="evenodd" d="m3.087 9 .54 9.176A3 3 0 0 0 6.62 21h10.757a3 3 0 0 0 2.995-2.824L20.913 9H3.087Zm6.133 2.845a.75.75 0 0 1 1.06 0l1.72 1.72 1.72-1.72a.75.75 0 1 1 1.06 1.06l-1.72 1.72 1.72 1.72a.75.75 0 1 1-1.06 1.06L12 15.685l-1.72 1.72a.75.75 0 1 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                </svg>
+                ')
+                ->id()
+                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
+                ->dispatch('delete', ['rowId' => $row->id])
         ];
     }
 
-    /*
-    public function actionRules($row): array
+    public function header(): array
     {
-       return [
-            // Hide button edit for ID 1
-            Rule::button('edit')
-                ->when(fn($row) => $row->id === 1)
-                ->hide(),
+        return [
+            Button::add('add-penukaran')
+                ->slot(__('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#10b981" class="w-5 h-5">
+                <path fill-rule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875ZM12.75 12a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V18a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V12Z" clip-rule="evenodd" />
+                <path d="M14.25 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 16.5 7.5h-1.875a.375.375 0 0 1-.375-.375V5.25Z" />
+                </svg>
+                '))
+                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700 w-full')
+                ->openModal('penukaran-form', []),
+            Button::add('export-pdf')
+                ->slot(__('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#10b981" class="w-5 h-5">
+                <path fill-rule="evenodd" d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z" clip-rule="evenodd" />
+                </svg>'))
+                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700 w-full')
+                ->dispatch('exportPdf', []),
+            Button::add('bulk-export-pdf')
+                ->slot(__('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#10b981" class="w-5 h-5">
+                <path fill-rule="evenodd" d="M9.75 6.75h-3a3 3 0 0 0-3 3v7.5a3 3 0 0 0 3 3h7.5a3 3 0 0 0 3-3v-7.5a3 3 0 0 0-3-3h-3V1.5a.75.75 0 0 0-1.5 0v5.25Zm0 0h1.5v5.69l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 1 1 1.06-1.06l1.72 1.72V6.75Z" clip-rule="evenodd" />
+                <path d="M7.151 21.75a2.999 2.999 0 0 0 2.599 1.5h7.5a3 3 0 0 0 3-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 0 1-4.5 4.5H7.151Z" />
+                </svg>(<span class="text-emerald-500 fill-emerald-500" x-text="window.pgBulkActions.count(\'' . $this->tableName . '\')"></span>)'))
+                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700 w-full')
+                ->dispatch('bulkExportPdf', []),
+
         ];
     }
-    */
+
+    protected function getListeners()
+    {
+        return array_merge(
+            parent::getListeners(),
+            [
+                'exportPdf',
+                'bulkExportPdf',
+                'delete',
+                'penukaranUpdated' => '$refresh',
+            ]
+        );
+    }
+
+    // Function to export PDF using DomPDF
+    public function exportPdf()
+    {
+        $path = public_path() . '/pdf';
+        // Mendapatkan datasource
+        $datasource = $this->datasource()->get();
+        // Membuat folder pdf jika belum ada
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+        // Membuat file pdf
+        $pdf = Pdf::loadView('pdf.penukaran', ['datasource' => $datasource]);
+        // Menyimpan file pdf ke folder pdf
+        $pdf->save($path . '/penukaran.pdf');
+        // Menampilkan file pdf
+        return response()->download($path . '/penukaran.pdf');
+
+        // Delete the file after download
+    }
+
+    public function bulkExportPdf()
+    {
+        // Get the selected rows
+        $selectedRows = $this->datasource()->whereIn('penukaran.id', $this->checkboxValues)->get();
+
+        $path = public_path() . '/pdf';
+        // Membuat folder pdf jika belum ada
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+        // Membuat file pdf
+        $pdf = Pdf::loadView('pdf.penukaran', ['datasource' => $selectedRows]);
+        // Menyimpan file pdf ke folder pdf
+        $pdf->save($path . '/penukaran.pdf');
+        // Menampilkan file pdf
+        // return response()->download($path . '/penukaran.pdf');
+
+        // Download the file
+        $response = response()->download($path . '/penukaran.pdf');
+
+        DeleteFile::dispatch($path . '/penukaran.pdf')->delay(now()->addMinutes(1));
+
+        return $response;
+    }
+
+    // Function to delete data
+    public function delete($rowId)
+    {
+        $penukaran = Penukaran::findOrFail($rowId);
+        $tabungan = $penukaran->tabungan;
+
+        $historyTabungan = HistoryTabungan::where('tabungan_id', $tabungan->id)
+            ->where('keterangan', 'Penukaran barang')
+            ->where('created_at', $penukaran->created_at)
+            ->first();
+
+        if ($historyTabungan) {
+            $tabungan->saldo += $historyTabungan->kredit;
+            $tabungan->save();
+
+            $historyTabungan->delete();
+        }
+
+        $penukaran->delete();
+        $this->success('Penukaran berhasil dihapus dan saldo telah dikembalikan');
+    }
 }
